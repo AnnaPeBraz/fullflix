503aab47beba281b5b6fbd0af5d41954254bbb62
AnnaPeBraz <annap.0103@gmail.com>
2022-07-18 18:42:58 -0300

    add footer e main

--- src/paginas/home.jsx
+++ src/paginas/home.jsx
@@ -1,8 +1,12 @@
-export function Inicial(){
+import { Footer } from "../Componentes/footer";
+
+export default function Inicial(){
     return( // return sempre retorna html, css ou jsx
     //jsx precisa de uma div pai.
         <div> 
-                        
+           <div>Div da Imagem inicial</div>
+           <div>Div dos Cards</div>
+           <Footer/>             
         </div>
     )
 }
