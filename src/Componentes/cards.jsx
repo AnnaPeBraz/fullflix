113c2d9670e4c7d3f38c594f86d15c0901c057f2
AnnaPeBraz <annap.0103@gmail.com>
2022-07-19 20:35:36 -0300

    preparação de cards apra receber as infos da api

--- src/Componentes/cards.jsx
+++ src/Componentes/cards.jsx
@@ -1,7 +1,27 @@
-
+import { useEffect, useState } from "react"
+import { Pegarfilmes } from "../assets/apis/api"
 
 export function Cards (){
+    const [filmes, setFilmes] = useState();
+
+    useEffect(()=>{
+        Pegarfilmes(setFilmes)
+    },[])
+    
     return(
-        <div>Cards</div>
+        <div>
+            {!filmes ? <></> : <>{filmes.map((filme)=>{
+                return(
+                    <div>
+                        <img></img>
+                        <p></p>
+                        <p></p>
+               
+                    </div>
+                  ) 
+                
+                })}</>
+            }
+        </div>
     )
 }
