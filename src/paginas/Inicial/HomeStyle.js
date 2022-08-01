f52b2be7c890a6ec7fdb3d5bfb4227b33a967030
AnnaPeBraz <annap.0103@gmail.com>
2022-08-01 19:22:17 -0300

    arrumando o H2 - titulo

--- src/paginas/Inicial/HomeStyle.js
+++ src/paginas/Inicial/HomeStyle.js
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 import imagemBG from "./../../assets/images/cabeca.png"
 
-export const Titulo = styled.h2 `
+export const Titulo = styled.h2  `
     color: white;
     font-size: 40px;
     background-color: black;
     text-align: center;
+    height: 10vh;
+    /* margin: 0; */
+    /* h2{
+        margin: 0;
+    } */
 
 `
 
@@ -23,6 +28,12 @@ export const Pai = styled.div `
 `
 export const p1 = styled.h2 `
     background-color: black;
+    height: 10vh;
+    margin: 0;
+    h2{
+        margin: 0;
+
+    }
 `
 export const p2 = styled.div `
     display
