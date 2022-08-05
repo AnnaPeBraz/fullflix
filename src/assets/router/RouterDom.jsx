f37954c547d2ea26a81a988658d7ac4d09fc5a05
AnnaPeBraz <annap.0103@gmail.com>
2022-08-05 13:40:49 -0300

    salvando a ultima versao pq vai que eu esqueci alguma coisa

--- src/assets/router/RouterDom.jsx
+++ src/assets/router/RouterDom.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Detalhes } from "../../paginas/Detalhes/Detalhes";
 import Home from "../../paginas/Inicial/Home"
+import {Link} from "react-router-dom"
 
 
 export function RouterDom(){
@@ -9,6 +10,7 @@ export function RouterDom(){
             <Routes>
                 <Route index element={<Home/>}/>
                 <Route path="/detalhe" element={<Detalhes/>}/>
+                <Route path="*" element={<Link to="/"> <button>ERRO 404 - Pag não encontrada. Clique aqui para Voltar :)</button></Link>}/>
             </Routes>
         </BrowserRouter>
     )
